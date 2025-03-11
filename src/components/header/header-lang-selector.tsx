a898112c4e2a9883c3c3027e1bceeceb8a9be79f
kurisuamadeus <82453112+kurisuamadeus@users.noreply.github.com>
2025-03-11 15:02:14 +0900

    Fix Store Data Not Persist storeのデータが永続化されない
    English:
    - Add validation in client side component that use useeffect to validate and only run if zustand store is hydrated
    - Modify store.tsx to make it persist and save data to localstorage
    日本語:
    - useeffectを使うクライアント側のコンポネントをzustandのhydrated状態のバリデーションを追加
    - zustandのstoreを永続化されるためにstore.tsxを変更（zustandのstoreデータをlocalstorageに保存）

--- src/components/header/header-lang-selector.tsx
+++ src/components/header/header-lang-selector.tsx
@@ -1,37 +1,41 @@
 import { useLanguageStore } from "@/store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { languageAvailable } from "@/utils/helper";
 
 
 export default function HeaderLangSelector() {
     const { lang, setLang } = useLanguageStore();
-
-
+    const [isHydrated, setIsHydrated] = useState(false)
     // run on component mount
     useEffect(() => {
-        let currentUrl = []
-        currentUrl = window.location.href.split('/')
-        // if lang is empty, set it
-        if (lang == '') {
-            // if language in url is valid, set lang to url language, else set to english
-            if (languageAvailable.includes(currentUrl[3])) {
-                setLang(currentUrl[3]);
-            } else {
-                setLang('en');
+        if (useLanguageStore.persist.hasHydrated()) {
+            setIsHydrated(true)
+        }
+        if (isHydrated) {
+            let currentUrl = []
+            currentUrl = window.location.href.split('/')
+            // if lang is empty, set it
+            if (lang == '') {
+                // if language in url is valid, set lang to url language, else set to english
+                if (languageAvailable.includes(currentUrl[3])) {
+                    setLang(currentUrl[3]);
+                } else {
+                    setLang('en');
+                }
+            }
+            // if language url is different from lang and language url is valid, set lang to url language
+            if (currentUrl[3] != lang) {
+                if (languageAvailable.includes(currentUrl[3])) {
+                    setLang(currentUrl[3]);
+                } else {
+                    // set language url to lang value if url language is invalid and do redirect
+                    currentUrl[3] = lang
+                    window.location.href = currentUrl.join('/')
+                    // router.push(currentUrl.join('/'));
+                }
             }
         }
-        // if language url is different from lang and language url is valid, set lang to url language
-        if (currentUrl[3] != lang) {
-            if (languageAvailable.includes(currentUrl[3])) {
-                setLang(currentUrl[3]);
-            } else {
-                // set language url to lang value if url language is invalid and do redirect
-                currentUrl[3] = lang
-                window.location.href = currentUrl.join('/')
-                // router.push(currentUrl.join('/'));
-            }
-        }
-    }, [])
+    }, [isHydrated])
     return <div className='absolute top-0 right-[3vw] pt-[2vh] text-black'>
         <select className="w-[7vw] h-[7vw] max-w-[100px] max-h-[100px] font-[min(2vw, 25px)] appearance-none rounded-[20%] text-align-last-center" onChange={(e) => {
             setLang(e.target.value)
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface LanguageStore {
     lang: string,
+    isHydrated: boolean,
     setLang: (lang: string) => void
 }
 
 
-export const useLanguageStore = create<LanguageStore>((set) => ({
+export const useLanguageStore = create<LanguageStore>()(persist((set) => ({
     lang: '',
+    isHydrated: false,
     setLang: (lang: string) => set({ lang })
-}))
+}), {
+    name: 'lang'
+}));
